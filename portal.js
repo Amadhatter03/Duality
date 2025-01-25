72852bd61714e173f4705b76b41a772f9e994259
tianp1n <pinedchr1@gmail.com>
2025-01-25 14:09:47 -0800

    Created portal.js

---
+++ portal.js
@@ -0,0 +1,13 @@
+class Portal {
+    constructor(game, x, y) {
+        Object.assign(this, {game, x, y});
+        this.sprite = new Animator(ASSET_MANAGER.getAsset("./Sprites/Portal.png"),
+            0, 0, 32, 32, 6, 0.1);
+    }
+
+    update() {}
+
+    draw(ctx) {
+        this.sprite.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    }
+}
